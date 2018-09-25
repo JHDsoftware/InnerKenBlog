4ffccd24ffffe261d1b8074a6fb6df96af7ccdf7
Haodong Ju <juhaodong@gmail.com>
2018-09-25 08:42:31 +0800

    Initial
    初始化IkBlog项目

--- blog/src/app/app.component.spec.ts
+++ blog/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'InnerKenBlog'`, async(() => {
+  it(`should have as title 'blog'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('InnerKenBlog');
+    expect(app.title).toEqual('blog');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to InnerKenBlog!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to blog!');
   }));
 });
